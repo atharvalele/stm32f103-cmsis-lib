4da0411cabbb009f8af1e254e46da0d19da0718f
Ayush <ayush.sriv79@gmail.com>
2021-02-04 20:05:30 +0530

    device: gpio: implement gpio config functions
    - implemented gpio mode and speed function
    - implemented gpio output type function

---
+++ include/device/gpio.h
@@ -0,0 +1,50 @@
+/* GPIO Pin definitions */
+#define GPIO0		(1 << 0)
+#define GPIO1		(1 << 1)
+#define GPIO2		(1 << 2)
+#define GPIO3		(1 << 3)
+#define GPIO4		(1 << 4)
+#define GPIO5		(1 << 5)
+#define GPIO6		(1 << 6)
+#define GPIO7		(1 << 7)
+#define GPIO8		(1 << 8)
+#define GPIO9		(1 << 9)
+#define GPIO10		(1 << 10)
+#define GPIO11		(1 << 11)
+#define GPIO12		(1 << 12)
+#define GPIO13		(1 << 13)
+#define GPIO14		(1 << 14)
+#define GPIO15		(1 << 15)
+#define GPIO_ALL	0xFFFF
+
+/* GPIO Port Configuration */
+/* Input Mode MODE[1:0] = 00 */
+#define GPIO_CNF_ANALOG         0x00
+#define GPIO_CNF_FLOATING       0x01
+#define GPIO_CNF_PUPD           0x02
+#define GPIO_CNF_RES            0x03
+
+/* Output Mode MODE[1:0] > 00) */
+#define GPIO_CNF_PUSH_PULL      0x00
+#define GPIO_CNF_OD             0x01
+#define GPIO_CNF_AF_PUSH_PULL   0x02
+#define GPIO_CNF_AF_OD          0x03
+
+/* GPIO Port Mode */
+#define GPIO_INPUT              0x00
+#define GPIO_OUTPUT_10MHz       0x01
+#define GPIO_OUTPUT_2MHz        0x02
+#define GPIO_OUTPUT_50MHz       0x03
+
+#define GPIO_SETT(n, sett) ((sett) << (4 * (n)))
+#define GPIO_SETT_MASK(n)  (0x03 << (4 * (n)))
+
+#define GPIO_CNF_SETT(n, sett) ((sett) << ((4 * (n)) + 2))
+#define GPIO_CNF_SETT_MASK(n)  (0x03 << ((4 * (n)) + 2))
+
+void gpio_config(void);
+void gpio_toggle(GPIO_TypeDef *port, uint16_t pins);
+void gpio_mode_set(GPIO_TypeDef *port, uint16_t pins, uint8_t mode, uint8_t pupd_sett);
+void gpio_output_options_set(GPIO_TypeDef *port, uint16_t pins, uint8_t otype_sett);
+void gpio_set(GPIO_TypeDef *port, uint16_t pins);
+void gpio_clear(GPIO_TypeDef *port, uint16_t pins);
---
+++ src/device/gpio.c
@@ -0,0 +1,126 @@
+#include "stm32f1xx.h"
+#include "device/gpio.h"
+
+void gpio_config()
+{
+    /* Enbale Clock */
+    SET_BIT(RCC->APB2ENR, RCC_APB2ENR_IOPCEN);
+    /* Set pin 13 as output with speed 2 MHz */
+    gpio_mode_set(GPIOC, GPIO13, GPIO_OUTPUT_2MHz, GPIO_CNF_PUSH_PULL);
+    /* Set output as Push pull */
+    gpio_output_options_set(GPIOC, GPIO13, GPIO_CNF_PUSH_PULL);
+}
+
+/* Toggle All GPIO pins passed as an argument */
+void gpio_toggle(GPIO_TypeDef *port, uint16_t pins)
+{
+    uint32_t portval;
+
+    portval = port->ODR ;
+    port->BSRR = (portval & pins) << 16 | (~portval & pins);
+}
+
+/* Set GPIO Pin Modes - I/P, O/P, Analog, AF  & Pull Up / Pull Down / Output Speed 10 MHz 
+ * 2 MHz / 50 MHz
+ */
+void gpio_mode_set(GPIO_TypeDef *port, uint16_t pins, uint8_t mode, uint8_t pupd_sett)
+{
+    uint8_t i;
+    uint32_t moder;
+    /*
+     * We only want to change the pins that are passed,
+     * so keep the original values and mask the passed ones
+     */
+
+    moder =  port->CRL;
+
+    for (i = 0; i < 8; i++) {
+        if (((1 << i) & pins) == 0) {
+            continue;
+        }
+
+        moder &= ~GPIO_SETT_MASK(i);
+        moder |= GPIO_SETT(i, mode);
+        if (mode == 0x00) {
+            moder &= ~GPIO_CNF_SETT_MASK(i);
+            moder |= GPIO_SETT(i, pupd_sett);
+        }
+
+    }
+
+    port->CRL = moder;
+
+    moder = port->CRH;
+
+    for (i = 8; i < 16; i++) {
+        if (((1 << i) & pins) == 0) {
+            continue;
+        }
+
+        /* In case of the CRH i.e. Configuration High Register the 
+         * value needs to be modded by 8 because the register is
+         * 32 bits only. The macro would expand to (4 * (8) + 2)
+         * for the first iteration however we need it to start from 0
+         * therefore the value passed is i % 8
+         */
+        moder &= ~GPIO_SETT_MASK(i % 8);
+        moder |= GPIO_SETT(i % 8, mode);
+        if (mode == GPIO_INPUT) {
+            moder &= ~GPIO_CNF_SETT_MASK(i % 8);
+            moder |= GPIO_CNF_SETT(i % 8, pupd_sett);
+        }
+    }
+
+    port->CRH = moder;
+}
+
+/* Set GPIO Output */
+void gpio_output_options_set(GPIO_TypeDef *port, uint16_t pins, uint8_t otype_sett)
+{
+    uint8_t i;
+    uint32_t otyper;
+    
+    otyper = port->CRL;
+
+    for (i = 0; i < 8; i++) {
+        if (((1 << i) & pins) == 0) {
+            continue;
+        }
+
+        otyper &= ~GPIO_CNF_SETT_MASK(i);
+        otyper |= GPIO_CNF_SETT(i, otype_sett);
+    }
+
+    port->CRL = otyper;
+
+    otyper = port->CRH;
+
+    for (i = 8; i < 16; i++) {
+        if (((1 << i) & pins) == 0) {
+            continue;
+        }
+        /* In case of the CRH i.e. Configuration High Register the 
+         * value needs to be modded by 8 because the register is
+         * 32 bits only. The macro would expand to (4 * (8) + 2)
+         * for the first iteration however we need it to start from 0
+         * therefore the value passed is i % 8
+         */
+        otyper &= ~GPIO_CNF_SETT_MASK(i % 8);
+        otyper |= GPIO_CNF_SETT(i % 8, otype_sett);
+    }
+
+    port->CRH = otyper;
+
+}
+
+/* Turn ON all GPIO pins passed as an argument */
+void gpio_set(GPIO_TypeDef *port, uint16_t pins)
+{
+    port->BSRR |= pins;
+}
+
+/* Turn OFF all GPIO pins passed as an argument */
+void gpio_clear(GPIO_TypeDef *port, uint16_t pins)
+{
+    port->BRR |= pins;
+}
