caf9b9adddeb0bc05947abdb7a2a01909ebe6895
Atharva Lele <itsatharva@gmail.com>
2021-02-07 13:08:41 +0530

    main: config gpio, nvic, usart
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
 #include "stm32f1xx.h"
 
+#include "device/gpio.h"
+#include "device/nvic.h"
 #include "device/rcc.h"
+#include "device/usart.h"
 
 void system_init(void)
 {
     rcc_config();
+    gpio_config();
+    usart_config();
+    nvic_config();
 }
 
 int main()
